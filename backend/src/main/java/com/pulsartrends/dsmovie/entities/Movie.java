8e580a29fa977702ef781fa766b6ed522c83e793
Rilson Almeida <rilson.almeida@gmail.com>
2022-01-12 09:27:29 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/pulsartrends/dsmovie/entities/Movie.java
@@ -0,0 +1,87 @@
+package com.pulsartrends.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+
+	
+	public Movie() {
+	}
+
+
+	public Movie(long id, String title, Double score, Integer count, String image) {
+		Id = id;
+		this.title = title;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+
+
+	public long getId() {
+		return Id;
+	}
+
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+
+	public Integer getCount() {
+		return count;
+	}
+
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
+	
+}
